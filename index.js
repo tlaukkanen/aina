67450b453c0ed9787600b9f7638bafca011c5eb4
Tommi Laukkanen <tlaukkanen@gmail.com>
2016-03-15 06:15:51 +0000

    Added support for DHT11 temperature sensor

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ var TelegramBot = require('node-telegram-bot-api');
 var config = require('./config.json');
 var request = require('request');
 var speak = require('speakeasy-nlp');
+var sensorLib = require('node-dht-sensor');
+
+var latestReadout = 0;
+var latestHumidity = 0;
+
+var sensor = {
+    initialize: function () {
+        return sensorLib.initialize(11, 4);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+	latestReadout = readout.temperature.toFixed(3);
+	latestHumidity = readout.humidity.toFixed(3);
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        setTimeout(function () {
+            sensor.read();
+        }, 60000);
+    }
+};
+
+
+if (sensor.initialize()) {
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
+
 
 var options = {
   polling: true
@@ -25,7 +53,7 @@ bot.onText(/.*what.*lunch.*\?/, function(msg, match) {
     var now = new Date();
     var year = now.getFullYear();
     var month = (now.getMonth()+1)<10 ? "0" + (now.getMonth()+1) : "" + (now.getMonth()+1);
-    var day = "10"; // now.getDate()<10 ? "0" + now.getDate() : "" + now.getDate();
+    var day = now.getDate()<10 ? "0" + now.getDate() : "" + now.getDate();
     request.get("http://www.sodexo.fi/ruokalistat/output/daily_json/134/"+year+"/"+month+"/"+day+"/fi",
         function(err, resp, body) {
             console.log("response: " + JSON.stringify(body));
@@ -49,5 +77,6 @@ function respond(original, message, chatId) {
     var info = JSON.stringify(classify); // "action: " + classify.action + " owner=" + classify.owner + " subject=" + classify.subject;
     var sentiment = speak.sentiment.analyze(original.text);
     info += " sentiment: " + sentiment.score;
+    info += "\ntemp: " + latestReadout + "'C humidity: " + latestHumidity + "%";
     bot.sendMessage(chatId, message + "\n" + info );               
 }
